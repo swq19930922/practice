7f4b699f7ab23f94061a06bf95fa7273c0be2214
swq <18552231848@163.com>
2019-03-29 23:21:45 -0700

    最长不重复子串

--- practice/Algorithm/LongestSubstring.py
+++ practice/Algorithm/LongestSubstring.py
@@ -8,14 +8,16 @@ def len_of_longest_substr(strs):
     left = 0            # 指示重复字符首次出现位置的下一位
     i = 0
     while i < lenth:
-        if strs[i] not in dict1:         # 添加未出现的字符到字典中
+        if strs[i] not in dict1:                    # 添加未出现的字符到字典中
             dict1[strs[i]] = i
         else:
-            left = max(left, dict1[strs[i]] + 1)
+            left = max(left, dict1[strs[i]] + 1)    # left不能倒退
             dict1[strs[i]] = i
         max_len = max(max_len, i - left + 1)
         i += 1
     return max_len
 
-strs = 'abba'
-print(len_of_longest_substr(strs))
+
+if __name__ == '__main__':
+    strs = 'abba'
+    print(len_of_longest_substr(strs))
