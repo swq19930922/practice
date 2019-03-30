f21ccd7656b1841aad2fbd8eb4ea02cbbed56cd3
swq <18552231848@163.com>
2019-03-29 23:28:50 -0700

    第一个不重复字符

--- practice/Algorithm/FirstNonRepeat.py
+++ practice/Algorithm/FirstNonRepeat.py
@@ -5,13 +5,15 @@ def first_non_repeat(string):
     n = len(string)
     char_count = {}
     for i in range(n):
-        if string[i] in char_count:
+        if string[i] in char_count:         # 第i个字符每出现一次计数加1
             char_count[string[i]] += 1
         else:
             char_count[string[i]] = 1
     for i in range(n):
-        if char_count[string[i]] == 1:
+        if char_count[string[i]] == 1:      # 遍历字符串找到对应计数为1的字符
             return string[i]
 
-string = 'aabcdeb'
-print(first_non_repeat(string))
+
+if __name__ == '__main__':
+    string = 'aabcdeb'
+    print(first_non_repeat(string))
