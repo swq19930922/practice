d5784e0e27ea434e57a61ad591678b5c39643801
swq <18552231848@163.com>
2019-03-29 22:58:27 -0700

    修改多少次可以使相邻字符不同

--- practice/Algorithm/DrawRepeat.py
+++ practice/Algorithm/DrawRepeat.py
@@ -1,21 +1,19 @@
 
-# 在python3.7下编写,不明白系统如何测试
-
 
 def draw_repeat(strs):
+    '''字符串去重，修改多少次可以使相邻字符不重复'''
     lenth = len(strs)
     count = 0
-    swap = False
-    i=1
+    swap = False            # 默认未修改
+    i = 1
     while i < lenth:
-        if strs[i] == strs[i-1] and not swap:
+        if strs[i] == strs[i-1] and not swap:   # 若与前一个元素相等且未修改，则计数加1，swap = True
             count += 1
             swap = True
-        elif strs[i] == strs[i-1] and swap:
-            i += 1
+        else:
             swap = False
-        elif strs[i] != strs[i-1]:
-            swap = False
+            if strs[i] == strs[i-1] and swap:     # 若与前一个元素相同但已修改，则跳过该元素
+                i += 1
         i += 1
     return count
 
